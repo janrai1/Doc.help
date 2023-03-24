7e4296455763bdb5ef353583faddc284e64a8889
Pratham Raj Bhatt <prathamrajbhatt456@gmail.com>
2023-03-25 02:15:10 +0530

    added login form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -8,7 +8,7 @@ const Register = () => {
   return (
   <div className="form-container">
     <Form layout="vertical" onFinish={onfinishHandler}>
-    <h1>Register From</h1>
+    <h1>Register Form</h1>
     <Form.Item label="Name" name="name">
      <Input type='text' required />
      </Form.Item>
