16c4978aed0a5440a74baa3d81af21716e441534
Pratham Raj Bhatt <prathamrajbhatt456@gmail.com>
2023-03-25 00:39:43 +0530

    registration form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import { Form, Input } from "antd"
 const Register = () => {
+  // form handler
+  const onfinishHandler = (values) => {
+    console.log(values)
+  }
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+  <div className="form-container">
+    <Form layout="vertical" onFinish={onfinishHandler}>
+    <h1>Register From</h1>
+    <Form.Item label="Name" name="name">
+     <Input type='text' required />
+     </Form.Item>
+     <Form.Item label="Email" name="email">
+     <Input type="email" required />
+     </Form.Item>
+     <Form.Item label="Name" name="name">
+     <Input type="password" required />
+     </Form.Item>
+     <button className="btn btn-primary" type="submit">
+      Register
+     </button>
+     
+    </Form>
+  </div>
   )
 }
 
