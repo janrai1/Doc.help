9d3cd152ef4b31499b7ee827df7ab0e7bce75e67
Shivansh Bhardwaj <96634814+bhardwajshivansh@users.noreply.github.com>
2023-03-25 18:12:43 +0530

    made login controller

--- controllers/userctrl.js
+++ controllers/userctrl.js
@@ -1,7 +1,8 @@
 const userModel= require('../models/userModels')
 const bcrypt = require('bcryptjs')
 
-const LoginController = () => {}
+const jwt = require('jsonwebtoken')
+
 //register callback
 const registerController = async(req,res) => {
     try {
@@ -23,6 +24,33 @@ const registerController = async(req,res) => {
     }
 
 }
+
+//login callback
+const LoginController = async (req,res) => {
+    try{
+
+        const user= await userModel.findOne({email:req.body.email})
+        if(!user){
+            return res.status(200).send({message:'user not found',success:false})
+        }
+        const isMatch = await bcrypt.compare(req.body.password,user.passsword)
+        if(!isMatch){
+            return res.status(200).send({message:'Invalid Email Or Password',success:false})
+        }
+
+        const token =jwt.sign({id:user.__id},process.env.JWT_SECRET,{expiresIn:'1d'})
+        res.status(200).send({message:'Login Success',success:true,token})
+
+    }  catch(error){
+        console.log(error);
+        res.status(500).send({message:`Error in login CTRL ${error.message}`})
+    }
+};
+
+
+
+
+
 
 module.exports = {
     LoginController,registerController
