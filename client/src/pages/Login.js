650c3248ec00e2fdc33f60a816ed736a7005ee66
Pratham Raj Bhatt <prathamrajbhatt456@gmail.com>
2023-03-26 18:11:59 +0530

    spinner added and redux checked

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/RegisterStyles.css';
 import { Form, Input ,message} from "antd"
+import { useDispatch } from 'react-redux';
+import { showLoading,hideLoading } from '../redux/features/alertSlice';
 import { Link,useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
@@ -8,11 +10,13 @@ import axios from 'axios';
 const Login = () => {
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   //form handler
   const onfinishHandler = async (values) => {
-  try{
-
+  try{ 
+    dispatch(showLoading());
     const res = await axios.post('/api/v1/user/login',values);
+    dispatch(hideLoading());
     if(res.data.success){
       localStorage.setItem('token',res.data.token);
       message.success('Login Succesafully');
@@ -22,6 +26,7 @@ const Login = () => {
     }
 
   }  catch(error){
+    dispatch(hideLoading());
     console.log(error);
     message.error('Something went wrong');
   }
