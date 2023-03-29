dd961ec36c1ac9d9702d3aff6a84b065687ad327
janrai1 <janhavirai2000@gmail.com>
2023-03-29 20:49:03 +0530

    layout setup

---
+++ client/src/Data/data.js
@@ -0,0 +1,22 @@
+export const SidebarMenu =[
+{
+    name:"Home",
+    path:"/",
+    icon:"fa-solid fa-house-chimney",
+},
+{name:"Appointments",
+path: "/appointments",
+icon:"fa-solid fa-list-ul"
+},
+{name:"Apply Doctor",
+path:"/apply-doctor",
+icon:"fa-solid fa-user-doctor"},
+{
+    name:"profile",
+    path:"/profile",
+    icon:"fa-regular fa-user"
+},
+{name:"Logout",
+path:"/Logout",
+icon:"fa-regular fa-right-from-bracket",},
+];
